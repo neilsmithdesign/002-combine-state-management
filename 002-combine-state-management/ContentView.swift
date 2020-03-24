ec47bafb7b41705e18483901e68e8070341e28b9
Neil Smith <neilsmithdesign@gmail.com>
2020-03-24 12:43:55 +0100

    Initial Commit

---
+++ 002-combine-state-management/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  002-combine-state-management
+//
+//  Created by Neil Smith on 24/03/2020.
+//  Copyright © 2020 Neil Smith Design LTD. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
